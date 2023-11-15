937de6b6668e98890e0733efc1408e647375c5e7
fadjodjas24 <miguelvieiraalmeida777@gmail.com>
2023-11-15 16:28:58 +0000

    Half functional
    work on the boundrys bug, make it hit the paddle.

--- Assets/Script_Pedro/Puck_Behaviour.cs
+++ Assets/Script_Pedro/Puck_Behaviour.cs
@@ -1,42 +1,66 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 public class Puck_Behaviour : MonoBehaviour
 {
-    public float speed = 5f;
+    public Transform leftWall;
+    public Transform rightWall;
+    public Transform topWall;
+    public Transform bottomtWall;
+    
+    public float forceMultiplier = 5f;
     public string inputAction = "Fire1"; // Change to the desired action
+    private Rigidbody rb;
 
-    private bool puckStarted = false;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        // Check if the specified action is triggered
-        if (Input.GetButtonDown(inputAction) && !puckStarted)
+        
+        if (Input.GetButtonDown(inputAction))
         {
-            StartMoving();
+            Vector3 v = new Vector3(Random.value, 0, Random.value) * forceMultiplier;
+            rb.AddForce(v);
         }
 
-        // Move the puck only if it has started
-        if (puckStarted)
+        Vector3 pp = transform.position;
+        if (pp.x > rightWall.position.x)
         {
-            MovePuck();
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(-v.x, v.y, v.z);
         }
+        
+        if (pp.x < leftWall.position.x)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(-v.x, v.y, v.z);
+        }
+        
+        
+        if (pp.z < bottomtWall.position.z)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(v.x, v.y, -v.z);
+        }
+        
+        
+        if (pp.z  > topWall.position.z)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(v.x, v.y, -v.z);
+        }
+
+        Vector3 vv = rb.velocity;
+
+        rb.velocity = vv * 0.99999f;
+
     }
 
-    void StartMoving()
-    {
-        puckStarted = true;
-        // Make the puck move forward along its local X-axis
-        GetComponent<Rigidbody>().velocity = transform.right * speed;
-    }
 
-    void MovePuck()
-    {
-        // You can add additional logic here for continuous movement if needed
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        // You can add additional logic here for collisions if needed
-    }
 }
---
+++ Assets/Script_Pedro/SyncPuckWithController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncPuckWithController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject paddle;
+    [SerializeField]
+    private GameObject controller;
+    
+    private bool followController = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(followController)
+        {
+            Vector3 pp = paddle.transform.position;
+            Vector3 cp = controller.transform.position;
+            paddle.transform.position = new Vector3(cp.x, pp.y, cp.z);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (controller == null)
+        {
+            controller = other.gameObject;
+            followController = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (controller != null && other.gameObject == controller)
+        {
+            controller = null;
+            followController = false;
+        }
+    }
+}
