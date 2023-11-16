64f7b6e092cc20834058070718395c76d01a1b7b
Pedro <pedro.miguel.luis.silva@gmail.com>
2023-11-16 17:44:24 +0000

    trying the colision with the paddle

--- Assets/Script_Pedro/Puck_Behaviour.cs
+++ Assets/Script_Pedro/Puck_Behaviour.cs
@@ -6,15 +6,23 @@ using Random = UnityEngine.Random;
 
 public class Puck_Behaviour : MonoBehaviour
 {
-    public Transform leftWall;
-    public Transform rightWall;
-    public Transform topWall;
-    public Transform bottomtWall;
+    [SerializeField]
+    private Transform leftWall;
+    [SerializeField]
+    private Transform rightWall;
+    [SerializeField]
+    private Transform topWall;
+    [SerializeField]
+    private Transform bottomtWall;
     
-    public float forceMultiplier = 5f;
-    public string inputAction = "Fire1"; // Change to the desired action
+    [SerializeField]
+    private float forceMultiplier = 5f;
+    [SerializeField]
+    private string inputAction = "Fire1"; // Change to the desired action
     private Rigidbody rb;
-
+    [SerializeField]
+    private GameObject paddle;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,45 +30,64 @@ public class Puck_Behaviour : MonoBehaviour
 
     void FixedUpdate()
     {
-        
         if (Input.GetButtonDown(inputAction))
         {
             Vector3 v = new Vector3(Random.value, 0, Random.value) * forceMultiplier;
             rb.AddForce(v);
         }
-
-        Vector3 pp = transform.position;
-        if (pp.x > rightWall.position.x)
-        {
-            Vector3 v = rb.velocity;
-            rb.velocity = new Vector3(-v.x, v.y, v.z);
-        }
+        colisionWalls();
+        colisionPaddle();
         
-        if (pp.x < leftWall.position.x)
-        {
-            Vector3 v = rb.velocity;
-            rb.velocity = new Vector3(-v.x, v.y, v.z);
-        }
-        
-        
-        if (pp.z < bottomtWall.position.z)
-        {
-            Vector3 v = rb.velocity;
-            rb.velocity = new Vector3(v.x, v.y, -v.z);
-        }
-        
-        
-        if (pp.z  > topWall.position.z)
-        {
-            Vector3 v = rb.velocity;
-            rb.velocity = new Vector3(v.x, v.y, -v.z);
-        }
-
         Vector3 vv = rb.velocity;
-
         rb.velocity = vv * 0.999f;
-    
+    }
 
+    void colisionPaddle()
+    {
+        Vector3 puckPosition = transform.position;
+        //if ((puckPosition.x + transform.localScale.x < paddle.transform.position.x + paddle.transform.localScale.x))
+        //{
+            //Vector3 v = rb.velocity;
+            //rb.velocity = new Vector3(-v.x, v.y, -v.z);
+        //}
+
+        if ((puckPosition.z + transform.localScale.z < paddle.transform.position.z + paddle.transform.localScale.z))
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(v.x, v.y, -v.z);
+        }
+        
+        
+    }
+
+    void colisionWalls()
+    {
+        Vector3 puckPosition = transform.position;
+        if (puckPosition.x + transform.localScale.x > rightWall.position.x)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(-v.x, v.y, v.z);
+        }
+        
+        if (puckPosition.x - transform.localScale.x < leftWall.position.x)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(-v.x, v.y, v.z);
+        }
+        
+        
+        if (puckPosition.z - transform.localScale.z < bottomtWall.position.z)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(v.x, v.y, -v.z);
+        }
+        
+        
+        if (puckPosition.z + transform.localScale.z> topWall.position.z)
+        {
+            Vector3 v = rb.velocity;
+            rb.velocity = new Vector3(v.x, v.y, -v.z);
+        }
     }
 
 
