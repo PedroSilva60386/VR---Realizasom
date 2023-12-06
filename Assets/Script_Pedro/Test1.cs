4a5062bcc51a5f63fc8624023a4d085950f50e90
Pedro <pmlu.silva@campus.fct.unl.pt>
2023-12-06 12:10:29 +0000

    test1 working

--- Assets/Script_Pedro/Test1.cs
+++ Assets/Script_Pedro/Test1.cs
@@ -17,13 +17,16 @@ public class Test1 : MonoBehaviour
     private Rigidbody _rb;
     private int _countAction;
     private int _paddleHits;
-    private bool _test1;
-    private bool _test2;
-    private bool _test3;
     private Vector3 _pos1;
     private Vector3 _pos2;
     private Vector3 _pos3;
     private Vector3 _posInitial;
+    private enum TestePhase
+    {
+        firstPhase, secondPhase, thirdPhase
+    }
+
+    private TestePhase testePhase;
 
 
     // Start is called before the first frame update
@@ -31,26 +34,23 @@ public class Test1 : MonoBehaviour
     { 
         _rb = GetComponent<Rigidbody>();
         _countAction = 0;
-        _test1 = false;
-        _test2 = false;
-        _test3 = false;
+        testePhase = TestePhase.firstPhase;
         _paddleHits = 0;
         _pos1 = new Vector3(-0.556f, 0.7998f, 1.351f);
         _pos2 = new Vector3(0.045f, 0.7998f, 1.351f);
-        _pos3 = new Vector3(-0.018f, 0.7998f, 0.188f);
+        _pos3 = new Vector3(0.556f, 0.7998f, 1.351f);
         _posInitial = transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (!_test1)
+        if (testePhase == TestePhase.firstPhase)
         {
             if (Input.GetButtonDown(inputAction))
             {
-                transform.position = _pos1;
+                //transform.position = _pos1;
                 _countAction++;
-                print("First action " +  _countAction);
                 if (_countAction == 2)
                 {
                     var v = new Vector3(0, 0, -20f) * forceMultiplier;
@@ -60,29 +60,35 @@ public class Test1 : MonoBehaviour
                 }
             }
         }
-        if (_test1 && !_test2)
+        else if (testePhase == TestePhase.secondPhase)
         {
-            transform.position = _pos2;
-            _countAction++;
-            print("First action 2 " +  _countAction);
-            if (_countAction == 2)
+            if (Input.GetButtonDown(inputAction))
             {
-                var v = new Vector3(0, 0, -20f) * forceMultiplier;
-                _rb.AddForce(v);
-                _countAction = 0;
-                
+                //transform.position = _pos2;
+                //_rb.velocity = Vector3.zero;
+                _countAction++;
+                if (_countAction == 2)
+                {
+                    var v = new Vector3(0, 0, -20f) * forceMultiplier;
+                    _rb.AddForce(v);
+                    _countAction = 0;
+                                
+                }
             }
         }
-        if (_test1 && _test2 && !_test3)
+        else if (testePhase == TestePhase.thirdPhase)
         {
-            transform.position = _pos3;
-            _countAction++;
-            if (_countAction == 2)
+            if (Input.GetButtonDown(inputAction))
             {
-                var v = new Vector3(0, 0, -20f) * forceMultiplier;
-                _rb.AddForce(v);
-                _countAction = 0;
-                
+                //transform.position = _pos3;
+                _countAction++;
+                if (_countAction == 2)
+                {
+                    var v = new Vector3(0, 0, -20f) * forceMultiplier;
+                    _rb.AddForce(v);
+                    _countAction = 0;
+             
+                }
             }
         }
     }
@@ -97,35 +103,31 @@ public class Test1 : MonoBehaviour
         if (c.gameObject.name == paddle.name)
         {
             _paddleHits++;
-            if (_paddleHits == 1)
+            print(_paddleHits);
+            switch (_paddleHits)
             {
-                _test1 = true;
-                ResetGame(_pos2);
-                Debug.Log("Test1 passed");
+                case 1:
+                    testePhase = TestePhase.firstPhase;
+                    ResetGame(_pos2);
+                    Debug.Log("Test1 passed");
+                    break;
+                case 2:
+                    testePhase = TestePhase.secondPhase;
+                    ResetGame(_pos3);
+                    Debug.Log("Test2 passed");
+                    break;
+                case 3:
+                    testePhase = TestePhase.thirdPhase;
+                    ResetGame(_posInitial);
+                    Debug.Log("Test3 passed");
+                    break;
             }
-            if (_paddleHits == 2)
-            {
-                _test2 = true;
-                ResetGame(_pos3);
-                Debug.Log("Test2 passed");
-            }
-            if (_paddleHits == 3)
-            {
-                _test3 = true;
-                ResetGame(_posInitial);
-                Debug.Log("Test3 passed");
-            }
-            
         }
-        
-        
-       
     }
     
      private void ResetGame(Vector3 position)
      {
          transform.position = position;
          _rb.velocity = Vector3.zero;
-    
      }
 }
